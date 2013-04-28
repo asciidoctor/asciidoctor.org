02a118a05abfc327bdaa7a67caa1e04c5b4d4f3a
Dan Allen <dallen@redhat.com>
2013-04-27 22:21:24 -0600

    increase # of news entries per page

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,8 +3,8 @@ require 'zurb-foundation'
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/news', :posts, nil, nil, :default_layout => 'post')
-  extension Awestruct::Extensions::Paginator.new(:posts, '/news', :per_page => 5)
-  extension Awestruct::Extensions::Tagger.new(:posts, '/news', '/news/tags', :per_page => 5)
+  extension Awestruct::Extensions::Paginator.new(:posts, '/news', :per_page => 10)
+  extension Awestruct::Extensions::Tagger.new(:posts, '/news', '/news/tags', :per_page => 10)
   #extension Awestruct::Extensions::TagCloud.new( :posts, '/news/tags/index.html')
   # Indexifier *must* come before Atomizer
   extension Awestruct::Extensions::Indexifier.new
