55e0e192bf3924a852c2ad0ac2f4514c516b4ff2
Dan Allen <dan.j.allen@gmail.com>
2014-08-11 03:31:02 -0600

    add Google Analytics extension, apply to user manual

--- _ext/awestruct_ext.rb
+++ _ext/awestruct_ext.rb
@@ -7,6 +7,31 @@ Asciidoctor::Extensions.register do
   if (docfile = @document.attributes['docfile'])
     @document.instance_variable_set :@base_dir, File.dirname(docfile)
   end
+
+  unless ::Awestruct::Engine.instance.development?
+    postprocessor {
+      process {|doc, output|
+        next output if (doc.attr? 'page-layout') || !(doc.attr? 'site-google_analytics_account')
+        account_id = doc.attr 'site-google_analytics_account'
+        %(#{output.rstrip.chomp('</html>').rstrip.chomp('</body>').chomp}
+<script>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount','#{account_id}']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+</script>
+</body>
+</html>)
+    }
+  }
+  end
 end
 
 module Awestruct
