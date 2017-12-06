c805ca2071c4c743a6f8e2174889d810f3d34fd2
Dan Allen <dan.j.allen@gmail.com>
2017-12-05 23:52:38 -0700

    add Asciidoctor extension to support remote nested include
    - add include preprocessor that detects and supports a remote nested include
    - assign current document to local var to make it accessible to handles? method of include processor
    - format source code

--- _ext/awestruct_ext.rb
+++ _ext/awestruct_ext.rb
@@ -15,11 +15,25 @@ end
 #OpenURI::Cache.cache_path = ::File.join Awestruct::Engine.instance.config.dir, 'vendor', 'uri-cache'
 
 Asciidoctor::Extensions.register do
-  # workaround for Awestruct 0.5.5
-  # (change lib/awestruct/handlers/asciidoctor_handler.rb, line 108 to opts[:base_dir] = @site.config.dir unless opts.key? :base_dir)
-  if (docfile = @document.attributes['docfile'])
-    @document.instance_variable_set :@base_dir, File.dirname(docfile)
+  current_document = @document
+
+  # workaround lack of docfile support for Asciidoctor base_dir option in Awestruct
+  if (docfile = current_document.attributes['docfile'])
+    current_document.instance_variable_set :@base_dir, (File.dirname docfile)
   end
+
+  # workaround lack of support for nested remote includes in Asciidoctor
+  include_processor do
+    handles? do |target|
+      current_document.reader.cursor.dir.start_with? 'https://'
+    end
+
+    process do |doc, reader, target, attrs|
+      inc_path = %(#{current_document.reader.cursor.dir}/#{target})
+      inc_contents = open(inc_path, 'r') {|f| f.read }
+      reader.push_include inc_contents, inc_path, target, 1, attrs
+    end
+  end unless current_document.options[:parse_header_only]
 
   preprocessor do
     process do |doc, reader|
@@ -27,22 +41,20 @@ Asciidoctor::Extensions.register do
       (doc.instance_variable_get :@attribute_overrides).delete 'prewrap'
       reader
     end
-  end unless @document.options[:parse_header_only]
+  end unless current_document.options[:parse_header_only]
 
-  unless ::Awestruct::Engine.instance.development?
-    postprocessor {
-      process {|doc, output|
-        next output if (doc.attr? 'page-layout') || !(doc.attr? 'site-google_analytics_account')
-        account_id = doc.attr 'site-google_analytics_account'
-        %(#{output.rstrip.chomp('</html>').rstrip.chomp('</body>').chomp}
+  postprocessor do
+    process do |doc, output|
+      next output if (doc.attr? 'page-layout') || !(doc.attr? 'site-google_analytics_account')
+      account_id = doc.attr 'site-google_analytics_account'
+      %(#{output.rstrip.chomp('</html>').rstrip.chomp('</body>').chomp}
 <script>
 !function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);}(window,document,'script','//www.google-analytics.com/analytics.js','ga'),ga('create','#{account_id}','auto'),ga('send','pageview');
 </script>
 </body>
 </html>)
-    }
-  }
-  end
+    end
+  end unless ::Awestruct::Engine.instance.development?
 end
 
 module Awestruct
