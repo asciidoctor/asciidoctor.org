2baf67b751824c16fcf389153a092dac539a2be4
Dan Allen <dan.j.allen@gmail.com>
2021-02-23 17:23:23 -0700

    fix indentation in URL router

--- _ext/routers/hack-asciidoctor-maven-plugin.js
+++ _ext/routers/hack-asciidoctor-maven-plugin.js
@@ -1,9 +1,9 @@
 var idMapping = {
-    '': '/maven-tools/latest/project/contributing/',
-    'hacking': '/maven-tools/latest/project/contributing/#hacking',
-    'building': '/maven-tools/latest/project/contributing/#hacking',
-    'testing': '/maven-tools/latest/project/contributing/#testing',
-    'resources': '/maven-tools/latest/project/contributing/#testing',
+  '': '/maven-tools/latest/project/contributing/',
+  'hacking': '/maven-tools/latest/project/contributing/#hacking',
+  'building': '/maven-tools/latest/project/contributing/#hacking',
+  'testing': '/maven-tools/latest/project/contributing/#testing',
+  'resources': '/maven-tools/latest/project/contributing/#testing'
 }
 
 var hash = window.location.hash
