7a7c3e794ab46ae6b59705f60e36e4b66d689fed
Dan Allen <dan.j.allen@gmail.com>
2021-09-01 00:52:27 -0600

    redirect online man page to new docs site

---
+++ _ext/routers/asciidoctor.js
@@ -0,0 +1,6 @@
+var idMapping = {
+  '': '/asciidoc/latest/cli/man1/asciidoctor/',
+}
+var hash = window.location.hash
+var url = idMapping[hash.substr(1)] || idMapping[''].concat(hash)
+window.location.href = (url.substr(0, 8) === 'https://' ? '' : 'https://docs.asciidoctor.org').concat(url)
